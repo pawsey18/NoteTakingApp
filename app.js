90fb001c36b7c6efba8dbc366b0a7f78dabebe5a
Nick Pawsey <pawsey18@gmail.com>
2021-09-13 12:31:44 -0300

    using module export and using the file

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-// Loading the module
-const fs = require('fs');
+// // Loading the module
+// const fs = require('fs');
 
-//fs.writeFileSync('notes.txt', 'My name is ');
+// //fs.writeFileSync('notes.txt', 'My name is ');
+
+// // challele: append a message to notes.txt
+
+// // 1. use AppendFileSync to append to the file
+// // 2. run the script
+// // 3. check work by opening the file and viewing the appended text
+
+// fs.appendFileSync('notes.txt', ' Jake Paul');
+
+// Importing npm Modules
+
+const name = require("./utils.js");
 
 
-// challele: append a message to notes.txt
 
-// 1. use AppendFileSync to append to the file
-// 2. run the script
-// 3. check work by opening the file and viewing the appended text
-
-fs.appendFileSync('notes.txt', ' Jake Paul');
+console.log(name);
